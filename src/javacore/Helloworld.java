4d25bb625e0e733697e53f5e9ad44068cd29f789
tranminhnghia01 <tranminhnghia.a01@gmail.com>
2024-09-29 19:05:41 +0700

    first commit

---
+++ src/javacore/Helloworld.java
@@ -0,0 +1,13 @@
+package javacore;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+
+	}
+
+}
